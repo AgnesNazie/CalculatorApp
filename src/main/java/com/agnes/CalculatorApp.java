2bc80df75586720a06f9312502b3529b937d3848
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-06 16:23:53 +0100

    Initial commit

---
+++ src/main/java/com/agnes/CalculatorApp.java
@@ -0,0 +1,35 @@
+package com.agnes;
+
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+
+        // create a method for addition
+        public double add ( double number1, double number2){
+            return number1 + number2;
+        }
+        public double subtract ( double number1, double number2){
+            return number1 - number2
+        }
+        public double multiply ( double number1, double number2){
+            return number1 * number2
+        }
+
+    }
+
+    public double divide(double number1, double number2) throws ArithmeticExceptions {
+        if (number2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return number1 / number2;
+
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            boolean continueCalculation = true;
+            while (continueCalculating) [
+            System.out.println("E");]
+        }
+    }
+
+}
---
+++ src/main/java/com/agnes/Main.java
@@ -0,0 +1,9 @@
+package com.agnes;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my Calculator App!");
+
+       }

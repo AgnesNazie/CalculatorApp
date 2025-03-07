85ba683a9c7bbc2c41554f3a06d307dd2a28d9b0
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-07 08:56:02 +0100

    updated main by creating my CalculatorApp as an object

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -1,9 +1,11 @@
 package com.agnes;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to my Calculator App!");
 
-       }
+        System.out.println("\n===================================");
+        System.out.println("  Welcome to My Calculator App! 🎉 ");
+        System.out.println("===================================\n");
+        CalculatorApp.main(args);
+    }
+}
